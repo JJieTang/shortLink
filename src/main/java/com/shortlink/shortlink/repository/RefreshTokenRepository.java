9ed6e8d0ed8ec9bc8fc8ea3b4fb5f78fd3736738
Jie Tang <jtangjessica@gmail.com>
2026-04-02 11:41:01 +0200

    add refresh token persistence

---
+++ src/main/java/com/shortlink/shortlink/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.shortlink.shortlink.repository;
+
+import com.shortlink.shortlink.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+}
